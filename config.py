60873380c81a188c4eacf7e7d7f2893da9aae99e
Rom1987 <maks3564195@gmail.com>
2022-07-18 17:44:50 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "1778886123:AAGdD9k1VlIsZR4Q2xLXaC5vhjaztrP_zdM"  # Your token
+URL_APPLES = "https://moscowfresh.ru/iabloki-sezonnye-p3042"
+URL_PEAR = "https://moscowfresh.ru/grusha-abbat-p562"
